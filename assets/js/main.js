f88fe9c728f329eddf7fa2a67cc96bba283207c3
guotingchaopr <guotingchaopr@gmail.com>
2014-05-23 18:10:37 +0800

    change btn class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
 $(function () {
-	$('.ui.sidebar').first().sidebar('attach events', '#showSidBtn');;
+    $('.ui.sidebar').first().sidebar('attach events', '#showSidBtn');
+    var handler;
+    $('#showSidBtn').first().sidebar('attach events', '.styled.example .button');
+
+    $('#showSidBtn').first().sidebar('attach events', '.floating.example .button');
 });
