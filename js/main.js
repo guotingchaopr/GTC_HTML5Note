cc8da03247585424b70cef9ec007e11be80c3587
guotingchaopr <guotingchaopr@gmail.com>
2014-07-22 13:42:08 +0800

    添加文章

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.semantic = {
+	handler: {}
+};
+semantic.ready = function () {
+	var menu = {};
+	menu = {
+		mouseenter: function () {
+			$(this).stop().animate({
+				width: '155px'
+			}, 300, function () {
+				$(this).find('.text').show();
+			});
+		},
+		mouseleave: function (event) {
+			$(this).find('.text').hide();
+			$(this).stop().animate({
+				width: '70px'
+			}, 300);
+		}
+	};
+	var $sidebarButton     = $("#showSidBtn"),
+       $uiSidebar         = $(".ui header .sidebar");
+		 $sidebarButton.on('mouseenter', menu.mouseenter).on('mouseleave', menu.mouseleave);
+		 $uiSidebar.sidebar('attach events', '#showSidBtn,.showAll');
+}
+$(function () {
+	semantic.ready();
+})
